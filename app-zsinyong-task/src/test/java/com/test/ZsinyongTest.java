f14446933471c473a7da76b51ef0099d29ad6891
qinyong <z1234561>
2018-09-29 10:33:56 +0800

    修改单元测试

--- app-zsinyong-task/src/test/java/com/test/ZsinyongTest.java
+++ app-zsinyong-task/src/test/java/com/test/ZsinyongTest.java
@@ -7,25 +7,18 @@ import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-/**
- * <b>@Author:</b>        jlliu3 <br/>
- * <b>@Version:</b>       1.0.1 <br/>
- * <b>@CreateTime:</b>    2018/2/27 16:31 <br/>
- * <b>@Copyright</b>     @Copyright (c) 2018 by iflytek <br/>
- * <b>@Description</b>   etl测试工具 <br/>
- */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath*:spring/spring-context.xml"})
-public class GclpEtlTest {
+public class ZsinyongTest {
 
-    Logger logger = LoggerFactory.getLogger(GclpEtlTest.class);
+    Logger logger = LoggerFactory.getLogger(ZsinyongTest.class);
 
     @Test
     public void readEpas() {
         logger.info("--------------定时任务开始--------------------");
-        synchronized (GclpEtlTest.class) {
+        synchronized (ZsinyongTest.class) {
             try {
-                GclpEtlTest.class.wait();
+                ZsinyongTest.class.wait();
             } catch (Throwable e) {
 
             }
